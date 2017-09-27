9559645649f6759b687dca0ee2ae1fa425bcd7d1
Bruno Antão <brunomantao@gmail.com>
2017-09-27 20:12:55 +0100

    commit log test

--- client/js/game.js
+++ client/js/game.js
@@ -8,6 +8,7 @@ gameState = {
         game.stage.backgroundColor = "#212121";
         game.stage.smoothed = false;
         game.canvas.oncontextmenu = function (e) { e.preventDefault(); }
+        console.log('test');
 
     },
 
