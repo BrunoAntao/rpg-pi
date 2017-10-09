1a29ba4f97ff27a385aac4545f47decbaf3fb369
Bruno Antão <brunomantao@gmail.com>
2017-10-09 18:56:37 +0100

    map

---
+++ client/js/entity.js
@@ -0,0 +1,15 @@
+class Entity extends Phaser.Sprite{
+
+    constructor(x, y, key, group) {
+
+        super(game, x, y, key)
+
+        this.scale.setTo(2,2);
+        this.anchor.setTo(0.5, 1);
+        this.smoothed = false;
+
+        game.add.existing(this);
+        group.add(this);
+    }
+
+}
