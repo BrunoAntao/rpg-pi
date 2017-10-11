a825462fafbbae8f3de1e88f831a335ca9e84648
Bruno Antão <brunomantao@gmail.com>
2017-10-11 19:04:54 +0100

    projectile rotation bugfix

--- client/js/map.js
+++ client/js/map.js
@@ -19,34 +19,34 @@ class Map {
         //new Entity(300, 300, 'cactus1', this.group);
         //new Entity(400, 400, 'cactus2', this.group);
 
-        let forestBiome = game.add.graphics(0, 0);
-        forestBiome.beginFill(0x00ff00);
-        forestBiome.lineStyle(2, 0xffffff, 1);
-        forestBiome.drawRect(0, 0, width/4, height);
-        forestBiome.endFill();
+        // let forestBiome = game.add.graphics(0, 0);
+        // forestBiome.beginFill(0x00ff00);
+        // forestBiome.lineStyle(2, 0xffffff, 1);
+        // forestBiome.drawRect(0, 0, width/4, height);
+        // forestBiome.endFill();
 
-        let fireBiome = game.add.graphics(0, 0);
-        fireBiome.beginFill(0xff0000);
-        fireBiome.lineStyle(2, 0xffffff, 1);
-        fireBiome.drawRect(width* 3/4, 0, width/4, height);
-        fireBiome.endFill();
+        // let fireBiome = game.add.graphics(0, 0);
+        // fireBiome.beginFill(0xff0000);
+        // fireBiome.lineStyle(2, 0xffffff, 1);
+        // fireBiome.drawRect(width* 3/4, 0, width/4, height);
+        // fireBiome.endFill();
 
-        let iceBiome = game.add.graphics(0, 0);
-        iceBiome.beginFill(0xffff00);
-        iceBiome.lineStyle(2, 0xffffff, 1);
-        iceBiome.drawRect(width/4, height/2, width/2, height/2);
-        iceBiome.endFill();
+        // let iceBiome = game.add.graphics(0, 0);
+        // iceBiome.beginFill(0xffff00);
+        // iceBiome.lineStyle(2, 0xffffff, 1);
+        // iceBiome.drawRect(width/4, height/2, width/2, height/2);
+        // iceBiome.endFill();
 
-        let desertBiome = game.add.graphics(0, 0);
-        desertBiome.beginFill(0x0000ff);
-        desertBiome.lineStyle(2, 0xffffff, 1);
-        desertBiome.drawRect(width/4, 0, width/2, height/2);
-        desertBiome.endFill();
+        // let desertBiome = game.add.graphics(0, 0);
+        // desertBiome.beginFill(0x0000ff);
+        // desertBiome.lineStyle(2, 0xffffff, 1);
+        // desertBiome.drawRect(width/4, 0, width/2, height/2);
+        // desertBiome.endFill();
 
-        this.biome.add(forestBiome);
-        this.biome.add(fireBiome);
-        this.biome.add(iceBiome);
-        this.biome.add(desertBiome);
+        // this.biome.add(forestBiome);
+        // this.biome.add(fireBiome);
+        // this.biome.add(iceBiome);
+        // this.biome.add(desertBiome);
 
     }
 
