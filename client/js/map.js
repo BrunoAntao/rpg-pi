6d2c7ec8c36bcda30890c49d668b7542d197e288
Bruno Antão <brunomantao@gmail.com>
2017-10-13 14:27:09 +0100

    sprite gen is now a map method

--- client/js/map.js
+++ client/js/map.js
@@ -5,19 +5,45 @@ class Map {
         this.entities = entities;
         game.world.resize(width, height);
 
+        this.numOfSrites = 10;
+
         this.width = width;
         this.height = height;
         this.group = group;
         this.biome = game.add.group();
 
         this.forestBiomeBorders = { x1: 0, y1: 0, x2: width/4, y2: height};
-        this.fireBiomeBorders = {x1: width*3/4, y1: 0, x2: width/4, y2: height};
-        this.iceBiomeBorders = {x1: width/4, y1: height/2, x2: width/2, y2: height/2};
-        this.desertBiomeBorders = {x1: width/4, y1: 0, x2: width/2, y2: height/2};
+        this.fireBiomeBorders = {x1: width*3/4, y1: 0, x2: width, y2: height};
+        this.iceBiomeBorders = {x1: width/4, y1: 0, x2: width*3/4, y2: height/2};
+        this.desertBiomeBorders = {x1: width/4, y1: height/2, x2: width*3/4, y2: height};
 
-        //new Entity(200, 200, 'tree1', this.group);
-        //new Entity(300, 300, 'cactus1', this.group);
-        //new Entity(400, 400, 'cactus2', this.group);
+        this.borders = [
+            
+        {biome: this.forestBiomeBorders, sprites: ['tree1', 'tree2', 'tree3']},
+        {biome: this.fireBiomeBorders, sprites: ['tree1', 'tree2', 'tree3'] },
+        {biome: this.desertBiomeBorders, sprites: ['cactus1', 'cactus2', 'cactus2']},
+        {biome: this.iceBiomeBorders, sprites: ['cactus1', 'cactus2', 'cactus2']}
+        
+        ];
+
+        this.entitiesToLoad = [];
+        
+        this.borders.forEach( function(biome) {
+
+            for(var i = 0; i < this.numOfSrites; i++){
+
+                this.entitiesToLoad.push(this.generateSprite(biome));
+
+            }
+
+        }, this);
+
+        this.entitiesToLoad.forEach( function(entity) {
+
+            new Entity(entity.x, entity.y, entity.key, this.group);
+
+        }, this)
+
 
         let forestBiome = game.add.graphics(0, 0);
         forestBiome.beginFill(0x00ff00);
@@ -58,6 +84,24 @@ class Map {
 
         })
 
+
+    }
+
+    generateSprite(border){
+        
+        let minX = Math.min(border.biome.x1, border.biome.x2) + 64;
+        let maxX = Math.max(border.biome.x1, border.biome.x2) - 64;
+
+        let minY = Math.min(border.biome.y1, border.biome.y2) + 64;
+        let maxY = Math.max(border.biome.y1, border.biome.y2) - 64;
+
+        let x = Math.floor(Math.random() * (maxX - minX + 1)) + minX;
+        let y = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
+
+        let spriteNum = Math.floor(Math.random() * 3);
+
+        return {x: x, y: y, key: border.sprites[spriteNum]};
+        
     }
 
 }
