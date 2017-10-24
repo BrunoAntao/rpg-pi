d3d5a0baeac1cf3c29508358cd0e6cad18118154
Nunecas <nuno192009@live.com.pt>
2017-10-24 22:34:17 +0100

    Reverted some tests due to issues

--- test/test.js
+++ test/test.js
@@ -1,8 +1,6 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
-var player = new Player(100, 100);
-
 describe('Server', () => {
 
     it('Status', () => {
@@ -14,37 +12,5 @@ describe('Server', () => {
     });
 });
 
-describe('Player', () =>{
-
-    describe('Movement', () =>{
-
-        it('movement up', () => {
-
-            player.y -= 5;
-            player.y.should.equal(95);
-
-        });
-
-        it('movement down', () => {
-
-            player.y += 5;
-            player.y.should.equal(105);
-
-        });
-
-        it('movement right', () =>{
 
 
-            player.x += 5;
-            player.x.should.equal(105);
-
-        });
-
-        it('movement left', () =>{
-
-            player.x -= 5;
-            player.x.should.equal(95);
-
-        });
-    })
-})
