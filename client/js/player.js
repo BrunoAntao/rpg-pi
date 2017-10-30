0d3f03dd703968697c92136f6b9eb23dd4f64d0f
Nunecas <nuno192009@live.com.pt>
2017-10-30 22:48:02 +0000

    Added primary test structure

--- client/js/player.js
+++ client/js/player.js
@@ -385,3 +385,5 @@ class Mage extends Player {
 
 }
 
+
+
--- test/test.js
+++ test/test.js
@@ -1,16 +1,41 @@
 var expect  = require('chai').expect;
 var request = require('request');
+var chai = require('chai');
+
+chai.use(require('chai-dom'));
+chai.use(require('chai-http'));
+
 
 describe('Server', () => {
 
     it('Status', () => {
         
             request('http://localhost:80' , function(error, response, body) {
+
+                expect(response.statusCode).to.equal(200);
                 
-                expect(res.statusCode).to.equal(200);
         });
     });
-});
+})
+
+describe('Game', () =>{
+
+    it('Player', ()=> {
+        
+        chai.request('http://localhost:80').get('/').end( function(err, res){
+        
+                
+        }) 
+    }); 
+    
+    it('Map', () => {
+
+
+    })
+})
+
+
+
 
 
 
