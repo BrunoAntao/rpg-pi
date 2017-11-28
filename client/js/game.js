794e3fefc356bb160415f6647fa2065078802423
Bruno Antão <brunomantao@gmail.com>
2017-11-28 18:53:00 +0000

    added skill multiplayer events

--- client/js/game.js
+++ client/js/game.js
@@ -141,6 +141,12 @@ gameState = {
 
             })
 
+            socket.on('player skill', function (data) {
+
+                global.enemies[data.id].skill(data.angle);
+
+            })
+
         });
 
     },
