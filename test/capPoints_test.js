bfa4ddd6535cccf8397576c0513b5ff42d2b178d
ZeroToDefault <nuno192009@live.com.pt>
2017-12-19 20:11:46 +0000

    Minor changes

--- test/capPoints_test.js
+++ test/capPoints_test.js
@@ -11,21 +11,28 @@ describe('Capture Points', () => {
     let capPoint =  new CapPoint(100, 100, 'desert');
 
     expect(capPoint).to.haveOwnProperty('range').and.to.equal(150);
-    expect(capPoint).to.haveOwnProperty('prog').and.to.equal(0);
-    expect(capPoint).to.haveOwnProperty('captured').and.to.equal(false);
-    expect(capPoint).to.haveOwnProperty('biome').and.to.equal('desert');
-    expect(capPoint).to.haveOwnProperty('spawnCount').and.to.equal(0);
 
+    expect(capPoint).to.haveOwnProperty('prog').and.to.equal(0);
+
+    expect(capPoint).to.haveOwnProperty('captured').and.to.equal(false);
+
+    expect(capPoint).to.haveOwnProperty('biome').and.to.equal('desert');
+
+    expect(capPoint).to.haveOwnProperty('spawnCount').and.to.equal(0);
 
   })
 
   it('#spawn()', () => {
 
-    let capPoint =  new CapPoint(100, 100, 'desert');
+    let capPoint =  new CapPoint(100, 100, 'ice');
 
-    capPoint.spawn();
+    for (let i = 0; i < 4; i++) {
 
-    expect(capPoint).to.haveOwnProperty('spawnCount').and.to.equal(1);
-    
+      capPoint.spawn();
+
+    }
+
+    expect(capPoint).to.haveOwnProperty('spawnCount').and.to.equal(4);
+
   })
 })
--- test/slime_test.js
+++ test/slime_test.js
@@ -11,10 +11,15 @@ describe('Slime', () => {
     let slime = new Slime();
 
     expect(slime).to.haveOwnProperty('target').and.to.equal(false);
+
     expect(slime).to.haveOwnProperty('isTarget').and.to.equal(false);
+
     expect(slime).to.haveOwnProperty('isIdle').and.to.equal(false);
+
     expect(slime).to.haveOwnProperty('health').and.to.equal(1);
+
     expect(slime).to.haveOwnProperty('growth').and.to.equal(1);
+
     expect(slime).to.haveOwnProperty('growthMax').and.to.equal(6);
 
   })
@@ -24,6 +29,8 @@ describe('Slime', () => {
     let slime = new Slime();
 
     let mage = new Mage(100, 100);
+
+    expect(mage).to.have.haveOwnProperty('score').and.to.equal(0);
 
     slime.damage(1, mage);
 
